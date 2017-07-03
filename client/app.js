6e773cfe60bb093c9e56624e20a38cd4f9cad7a2
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-07-03 14:01:21 -0300

    Muda estilo em navbar e carregar bibliotecas
    angular material, material icons e dependências

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate','ngAria', 'ngMaterial']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
