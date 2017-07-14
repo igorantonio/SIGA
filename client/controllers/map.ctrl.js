808622bdec7ff974d52a9eb494bb7efd68a39e72
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-07-14 07:13:01 -0300

    Try click events no map

--- client/controllers/map.ctrl.js
+++ client/controllers/map.ctrl.js
@@ -1,16 +1,41 @@
 angular.module('myApp')
-    .controller('MapController', ['$scope', 'GeoService', function($scope, GeoService) {
+    .controller('MapController', ['$scope', 'GeoService', 'olData', function($scope, GeoService, olData) {
 
         var self = this;
 
         self.geojsons = GeoService.getEdificiosJson();
 
         angular.extend($scope, {
-        center: {
-            "lat": -7.214455941427701,
-            "lon": -395.90871261099613,
-            "zoom": 17
-        }
-    });
+            center: {
+                "lat": -7.214455941427701,
+                "lon": -395.90871261099613,
+                "zoom": 17
+            },
+            layers: self.geojsons,
+            defaults: {
+                events: {
+                    map: [ 'singleclick', 'pointermove' ],
+                    layers: ['mousemove', 'singleclick']
+                },
+                controls: {
+                    zoom: true,
+                    rotate: true,
+                    attribution: true
+                },
+                interactions: {
+                    mouseWheelZoom: true
+                }
+            }
+        });
+
+        olData.getMap().then(function(map) {
+            $scope.$on('openlayers.layers.singleclick', function(event, feature) {
+                console.log(event);
+            });
+        });
+
+        $scope.$on('openlayers.layers.mousemove', function(event, feature) {
+            console.log('oi')
+        });
 
 }]);
