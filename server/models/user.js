361c80649cc4ee1e6eef6633dd5d9b10274916c2
gabrielmla <gabmla19@gmail.com>
2017-07-28 12:00:11 -0300

    Validate email

--- server/models/user.js
+++ server/models/user.js
@@ -3,14 +3,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
 var User = new Schema({
   username: {
-  	type: String,
-  	lowercase: true
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   password: {
-  	type: String
+    type: String
   },
   created: {
     type: Date,
