bec5ba6d3c6e502cba7ca9db0fdf32cfe97a3d32
Gabriela Motta <gabriela.oliveira@ccc.ufcg.edu.br>
2017-07-28 21:22:30 -0300

    5 passing (7s)

--- spec.js
+++ spec.js
@@ -12,6 +12,46 @@ describe('loading express', function () {
     .get('/')
     .expect(200, done);
   });
+  it('responds to /logout', function testSlash(done) {
+  request(server)
+    .get('/logout')
+    .expect(200, done);
+  });
+  it('responds to /status', function testSlash(done) {
+  request(server)
+    .get('/status')
+    .expect(200, done);
+  });
+  it('responds to /users', function testSlash(done) {
+  request(server)
+    .get('/users')
+    .expect(200, done);
+  });
+  it('responds to /edificio', function testSlash(done) {
+  request(server)
+    .get('/edificio')
+    .expect(200, done);
+  });
+  it('responds to /edificio/:edificio_id', function testSlash(done) {
+  request(server)
+    .get('/edificio/:edificio_id')
+    .expect(200, done);
+  });
+  it('responds to /estatistica/edificio/:edificio_id', function testSlash(done) {
+  request(server)
+    .get('/estatistica/edificio/:edificio_id')
+    .expect(200, done);
+  });
+  it('responds to /estatistica/setor/:setor', function testSlash(done) {
+  request(server)
+    .get('/estatistica/setor/:setor')
+    .expect(200, done);
+  });
+  it('responds to /map', function testSlash(done) {
+  request(server)
+    .get('/map')
+    .expect(200, done);
+  });
   it('404 everything else', function testPath(done) {
     request(server)
       .get('/foooo')
