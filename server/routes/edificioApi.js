22a551e6652655eeaab575e483865a988c14f361
gabrielmla <gabmla19@gmail.com>
2017-08-13 16:43:39 -0300

    vazamentos vou corrigir no desktop

--- server/routes/edificioApi.js
+++ server/routes/edificioApi.js
@@ -22,7 +22,7 @@ router.post('/edificio/:edificio_id/geolocalizacao', function(req, res) {
     });
 });
 
-/// Show (Consumo)
+/// Show/Index (Consumo)
 router.get('/edificio/:edificio_id/consumo', function(req, res) {
     Edificio.findById(req.params.edificio_id, function(error, edificio) {
              if (error){ 
@@ -92,7 +92,8 @@ router.put('/edificio/:edificio_id/consumo/:consumo_id', function(req, res) {
             consumosAtualizado = [];
             edificio.historicoConsumo.forEach(function(consumo) {
                 if (consumo._id == req.params.consumo_id) {
-                    if (req.body.data) consumo.data     = new Date(req.body.data);
+                    data = new Date(req.body.data);
+                    if (req.body.data) consumo.data       = data.setTime(data.getTime() + data.getTimezoneOffset() * 60 *1000);
                     if (req.body.consumo) consumo.consumo = req.body.consumo;
                 }
                 consumosAtualizado.push(consumo);
@@ -134,48 +135,36 @@ router.delete('/edificio/:edificio_id/consumo/:consumo_id', function(req, res) {
     });
 });
 
+// Index (Vazamento)
 router.get('/edificio/:edificio_id/vazamentos', function(req, res) {
     Edificio.findById(req.params.edificio_id, function(error, edificio) {
-         if (error){ 
-              res.status(400).json({err: error});
-            }
-              else{
-            res.json(edificio);
-          };
-        // Filtrar
-        //Checar com eles, se tiver dois vazamentos no mesmo dia. o que fazer? Juntar?
-        vazamentosFiltrados = edificio.vazamentos;
-        res.json(vazamentosFiltrados);
-
-    })
-});
-
-router.delete('/edificio/:edificio_id/vazamentos/:vazamento_id', function(req, res) {
-    Edificio.findById(req.params.edificio_id, function(error, edificio) {
-         if (error){ 
-              res.status(400).json({err: error});
-          };
-        vazamentosFiltrados = [];
-        edificio.vazamentos.forEach(function(vazamento) {
-            if (vazamento._id != req.params.vazamento_id) {
-                vazamentosFiltrados.push(vazamento);
-            }
-        })
-        edificio.vazamentos = vazamentosFiltrados;
-        edificio.save(function(error) {
-            if (error){ 
-              res.status(400).json({err: error});
-            } else {
-                res.json(edificio.vazamentos);
-            }
-
-        })
+        if (error){ 
+            res.status(400).json({err: error});
+        } else {
+            res.json(edificio.vazamentos);
+        };
     });
-
-
 });
 
+// Show (Vazamento)
+router.get('/edificio/:edificio_id/vazamentos/:vazamento_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(err, edificio) {
+        if (err) {
+            res.status(400).json({error: err});
+        } else {
+            vazamentosFiltrados = []
+            edificio.vazamentos.forEach(function(vazamento) {
+                if (vazamento._id == req.params.vazamento_id) {
+                    vazamentosFiltrados.push(vazamento);
+                    return;
+                }
+            });
+            res.status(200).json(vazamentosFiltrados);
+        }
+    });
+});
 
+// Create (Vazamento)
 router.post('/edificio/:edificio_id/vazamentos/new', function(req, res) {
     if (req.body.data == null) {
         res.status(400);
@@ -207,7 +196,186 @@ router.post('/edificio/:edificio_id/vazamentos/new', function(req, res) {
     });
 });
 
-// Create
+// Delete (Vazamento)
+router.delete('/edificio/:edificio_id/vazamentos/:vazamento_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(error, edificio) {
+         if (error){ 
+              res.status(400).json({err: error});
+          };
+        vazamentosFiltrados = [];
+        edificio.vazamentos.forEach(function(vazamento) {
+            if (vazamento._id != req.params.vazamento_id) {
+                vazamentosFiltrados.push(vazamento);
+            }
+        });
+        edificio.vazamentos = vazamentosFiltrados;
+        edificio.save(function(error) {
+            if (error){ 
+              res.status(400).json({err: error});
+            } else {
+                res.json(edificio.vazamentos);
+            }
+
+        });
+    });
+});
+
+// Index    (Alerta)
+router.get('/edificio/:edificio_id/alertas', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(err, edificios) {
+        if (err) {
+            res.status(400).json({error: err});
+        } else {
+            res.status(200).json(edificios.alertas);
+        }
+    });
+});
+
+// Show     (Alerta)
+router.get('/edificio/:edificio_id/alertas/:alerta_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(err, edificio) {
+        if (err) {
+            res.status(400).json({error: err});
+        } else {
+            alertasFiltrados = [];
+            edificio.alertas.forEach(function(alerta) {
+                if (alerta._id == req.params.alerta_id) {
+                    alertasFiltrados.push(alerta);
+                    return;
+                }
+            });
+            res.status(200).json(alertasFiltrados);
+        }
+    });
+});
+
+// Create   (Alerta)
+router.post('/edificio/:edificio_id/alertas/new', function(req, res) {
+    if (req.body.data == null) {
+        res.status(400).send("Body is empty");
+        return;
+    }
+
+    Edificio.findById(req.params.edificio_id, function(error, edificio) {
+        if (err) {
+            res.status(400).json({error: err});
+            return
+        };
+
+        data = new Date(req.body.data);
+        novoAlerta = {
+            data: data.setTime(data.getTime() + data.getTimezoneOffset() * 60 * 1000),
+            volume: req.body.checked
+        };
+
+        edificio.alertas.push(novoAlerta);
+        edificio.save(function(error) {
+            if (err) {
+                res.status(422).json({error: err.message});
+                return;
+            } else {
+                res.json(edificio.alertas);
+            }
+        });
+    });
+});
+
+// Update   (Alerta)
+router.put('/edificio/:edificio_id/alertas/:alerta_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(err, edificio) {
+        if (err) {
+            res.status(400).json({error: err});
+        } else {
+            alertasFiltrados = [];
+            edificio.vazamentos.forEach(function(alerta) {
+                if (alerta._id == req.params.alerta_id) {
+                    if (req.body.data) alerta.data       = req.body.data;
+                    if (req.body.checked) alerta.checked = req.body.checked;
+                }
+                alertasFiltrados.push(alerta);
+            });
+            edificio.alertas = alertasFiltrados;
+            edificio.save(function(err) {
+                if (err) {
+                    res.status(400).json({ error: err});
+                } else {
+                    res.status(200).json({ message: 'Alerta atualizado.'});
+                }
+            });
+        }
+    });
+});
+
+// Delete   (Alerta)
+router.delete('/edificio/:edificio_id/alertas/:alerta_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(err, edificio) {
+        if (err) {
+            res.status(400).json({error: err});
+        } else {
+            alertasFiltrados = [];
+            edificio.alertas.forEach(function(alerta) {
+                if (alerta._id != req.params.alerta_id) {
+                    alertasFiltrados.push(alerta);
+                }
+            });
+            edificio.alertas = alertasFiltrados;
+            edificio.save(function(err) {
+                if (err) {
+                    res.status(400).json({error: err});
+                } else {
+                    res.status(200).json({message: 'Alerta removido.'});
+                }
+            });
+        }
+    });
+});
+
+// Index (Edificio)
+router.get('/edificio', function(req, res) {
+    Edificio.find(function(err, edificios) {
+        if (req.query.setor != null) {
+            edificios = filtrarPorSetor(req.query.setor, edificios);
+        }
+        if (req.query.nivelAlerta != null) {
+            nivelAlerta = req.query.nivelAlerta;
+            if (nivelAlerta == "0") {
+                margem = 0.2;
+            } else if (nivelAlerta == "1") {
+                margem = 0.3;
+            };
+            var result = emAlerta(edificios, margem);
+            res.send(result);
+            return;
+        }
+        if (req.query.withAlerta) {
+            if (req.query.withAlerta == 'true') {
+                var result0 = emAlerta(edificios, 0.2);
+                var result1 = emAlerta(edificios, 0.3);
+                res.json({
+                    todos: edificios,
+                    alerta0: result0,
+                    alerta1: result1
+                });
+                return;
+            }
+        }
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(edificios);
+        }
+    });
+});
+
+// Show (Edificio)
+router.get('/edificio/:edificio_id', function(req, res) {
+    Edificio.findById(req.params.edificio_id, function(error, edificio) {
+        if (error) res.send(edificio);
+        res.json(edificio);
+    });
+});
+
+// Create (Edificio)
 router.post('/edificio', function(req, res) {
     var edificio = new Edificio();
     edificio.nome = req.body.nome;
@@ -230,10 +398,9 @@ router.post('/edificio', function(req, res) {
           res.send(error.message);}
         else res.json(edificio);
     });
-
 });
 
-// Uṕdate
+// Uṕdate (Edificio)
 router.put('/edificio/:edificio_id', function(req, res) {
     Edificio.findById(req.params.edificio_id, function(err, edificio) {
         if (req.body.nome) edificio.nome            = req.body.nome;
@@ -251,6 +418,14 @@ router.put('/edificio/:edificio_id', function(req, res) {
                 res.status(200).json({message: 'Edificio atualizado.'});
             }
         });
+    });
+});
+
+//Delete (Edificio)
+router.delete('/edificio/:edificio_id', function(req, res) {
+    Edificio.remove({ _id: req.params.edificio_id }, function(error) {
+        if (error) res.send(error);
+        res.json({ message: "Prédio removido!" });
     });
 });
 
@@ -298,81 +473,5 @@ var FindEdificio = function(edificio_id, res) {
     });
     return edificiores;
 };
-
-// Index
-router.get('/edificio', function(req, res) {
-    Edificio.find(function(err, edificios) {
-        if (req.query.setor != null) {
-            edificios = filtrarPorSetor(req.query.setor, edificios);
-        }
-        if (req.query.nivelAlerta != null) {
-            nivelAlerta = req.query.nivelAlerta;
-            if (nivelAlerta == "0") {
-                margem = 0.2;
-            } else if (nivelAlerta == "1") {
-                margem = 0.3;
-            };
-            var result = emAlerta(edificios, margem);
-            res.send(result);
-            return;
-        }
-        if (req.query.withAlerta) {
-            if (req.query.withAlerta == 'true') {
-                var result0 = emAlerta(edificios, 0.2);
-                var result1 = emAlerta(edificios, 0.3);
-                res.json({
-                    todos: edificios,
-                    alerta0: result0,
-                    alerta1: result1
-                });
-                return;
-            }
-        }
-        if (err) {
-            res.send(err)
-        } else {
-            res.json(edificios);
-        }
-    });
-});
-
-// Show
-router.get('/edificio/:edificio_id', function(req, res) {
-    Edificio.findById(req.params.edificio_id, function(error, edificio) {
-        if (error) res.send(edificio);
-        res.json(edificio);
-    });
-});
-
-router.post('/edificio/:edificio_id', function(req, res) {
-    Edificio.findById(req.params.edificio_id, function(error, edificio) {
-        if (error) res.send(edificio);
-        edificio.nome = req.body.nome;
-        edificio.descricao = req.body.descricao;
-        edificio.atividade = req.body.atividade;
-        edificio.geolocalizacao = req.body.geolocalizacao;
-        edificio.caracteristicasFisicas = req.body.caracteristicasFisicas;
-        edificio.historicoConsumo = req.body.historicoConsumo; // Pessoalmente eu acho melhor que essa linha não exista;
-        edificio.vazamentos = req.body.vazamentos;
-        edificio.save(function(error) {
-            if (error) res.send(error);
-            res.json(edificio);
-        });
-    });
-});
-
-//Delete
-router.route('/edificio/:edificio_id')
-    .delete(function(req, res) {
-        Edificio.remove({
-            _id: req.params.edificio_id
-        }, function(error) {
-            if (error) res.send(error);
-            res.json({
-                message: "Prédio removido!"
-            });
-        });
-    });
-
 
 module.exports = router;
