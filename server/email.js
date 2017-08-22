ccb71f71cc1ad3dcea4295f3b5eb05a2585425c8
Rafaela9136 <rafaela9136@gmail.com>
2017-08-21 22:45:45 -0300

    Envia email quando o prédio está em alerta

--- server/email.js
+++ server/email.js
@@ -1,7 +1,5 @@
 var nodemailer = require('nodemailer');
 
-var edificio = "none";
-
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({ 
     service: 'gmail',
@@ -11,19 +9,19 @@ var transporter = nodemailer.createTransport({
     secure: true
 });
 
-// setup email data with unicode symbols
-var mailOptions = {
-    from: '"SIGA - Sistema de Gerênciamente de Água" <siga@ufcg.com>', // sender address
-    to: 'rafaela9136@gmail.com', // list of receivers
-    subject: 'Hello', // Subject line
-    text: 'Its me you looking for? '  + edificio.nome, // plain text body
-    html: '<b>It is me you are looking for?</b>' // html body
-};
-
 // send mail with defined transport object
-
 exports.sendEmail = function(ed) {
-    edificio = ed;
+
+    // setup email data with unicode symbols
+    var mailOptions = {
+        from: '"SIGA - Sistema de Gerênciamente de Água" <siga@ufcg.com>', // sender address
+        to: 'rafaela9136@gmail.com', // list of receivers
+        subject: 'Consumo suspeito', // Subject line
+        text: "Olá!\n nós notamos que o prédio" + ed.nome + " está apresentando um consumo suspeito. Verifique assim que possível!\nCondialmente,\nSIGA", // plain text body
+        html: '<p>Olá!</p>' + '<p>nós notamos que o prédio ' + ed.nome + 
+              ' está apresentando um consumo suspeito. Verifique assim que possível!</p>'
+              + '<p>Cordialmente,</p>' + '<p>SIGA.</p>' // html body
+    };
 
     transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
